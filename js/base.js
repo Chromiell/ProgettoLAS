9636fee24d0cc36a28e5bc8967fee8f60dbf1485
Fabio Salvini <salvini.fabio001@gmail.com>
2014-12-27 15:05:22 +0100

    Aggiunta pagina itinerari sportivi

---
+++ js/base.js
@@ -0,0 +1,5 @@
+$(function () {
+  $("#navigation_bar").load("page_sections/navigation_bar.html");
+  $("#footer").load("page_sections/footer.html");
+  $("#rif-contacts-users").load("page_sections/rif-contacts-users.html");
+});
