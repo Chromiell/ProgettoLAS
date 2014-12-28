05fb3e5473a36c8d4bc984fc88f496deec5dd702
Fabio Salvini <salvini.fabio001@gmail.com>
2014-12-28 17:12:39 +0100

    Impostati tutti i link in relativi

--- js/base.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-  $("#navigation_bar").load("http://fsalvini.github.io/ProgettoWebDesign/page_sections/navigation_bar.html");
-  $("#footer").load("http://fsalvini.github.io/ProgettoWebDesign/page_sections/footer.html");
-  $("#rif-contacts-users").load("http://fsalvini.github.io/ProgettoWebDesign/page_sections/rif-contacts-users.html");
-});
